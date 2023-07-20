de93f42bd152fafa39b331b36f78f8653a5fc7c6
nestor12m <nestor20martinez@gmail.com>
2023-07-20 14:19:11 -0500

    version 0.1.1

---
+++ layout/header.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Colegio MBB</title>
+    <link rel="stylesheet" href="css/estilos_principal.css" />
+    <link
+      href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css"
+      rel="stylesheet"
+      integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM"
+      crossorigin="anonymous"
+    />
+  </head>
+  <body>
+    <header>
+      <div></div>
+      <div class="titulo">
+        <h1>COLEGIO MANUEL BELTRAN BOSQUE</h1>
+      </div>
+    </header>
+    <!-----------------------------------------------------------------------------------------
+                                              menu
+    --------------------------------------------------------------------------------------------- -->
+    <div class="menu_principal">
+      <nav class="navbar bg-primary" data-bs-theme="dark">
+        <!-- Navbar content -->
+
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#">Inicio</a>
+          <a class="navbar-brand" href="#">Nosotros</a>
+          <a class="navbar-brand" href="#">Comunidad Educativa</a>
+          <a class="navbar-brand" href="contacto.html">Contactanos</a>
+          <a class="navbar-brand" href="#">Iniciar Sesion</a>
+        </div>
+      </nav>
+    </div>
