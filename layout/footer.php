778dd228618b5d379287aa342a8e25bf22ad81a6
nestor12m <nestor20martinez@gmail.com>
2023-07-27 20:05:20 -0500

    0.0.1.4

---
+++ layout/footer.php
@@ -0,0 +1,52 @@
+ <!-----------------------------------------------------------------------------------------
+                                           footer
+    --------------------------------------------------------------------------------------------- -->
+
+    <div class="footer-propidades">
+      <footer>
+        <div class="row">
+          <!-- <nav class="navbar" data-bs-theme="dark">
+      
+
+            <div class="container-fluid">
+              <a class="navbar-brand" href="#">Inicio</a>
+              <a class="navbar-brand" href="#">Nosotros</a>
+              <a class="navbar-brand" href="#">Comunidad Educativa</a>
+              <a class="navbar-brand" href="#">Contactanos</a>
+              <a class="navbar-brand" href="#">Iniciar Sesion</a>
+            </div>
+          </nav> -->
+        </div>
+
+        <div class="row">
+          <div class="col-6">
+            Línea de Admisiones Bogotá: <br />
+            (601) 744 08 94 - 302 2907400<br />
+            Línea Gratuita 01 8000 180537<br /><br /><br />
+
+            Atención DIGITURNO TEAMS:<br />
+            Digiturnos Teams <br />
+          </div>
+          <div class="col-6">
+            Manual gestión PQRSDF y Requerimientos <br /><br />
+            Aspirantes <br /><br />
+            Estudientes <br /><br />
+            Graduados <br /><br />
+            Colaboradores<br /><br />
+          </div>
+        </div>
+        <div class="containercentro">
+          <div class="row">
+            <div class="c-redes">
+              <a href=""><img class="redes" src="img/facebook.svg" alt="" /></a>
+              <a href=""><img class="redes" src="img/twitter.svg" alt="" /></a>
+              <a href=""><img class="redes" src="img/youtube.svg" alt="" /></a>
+            </div>
+          </div>
+
+          <div class="row text-center">
+            Realizado por Nestor Martinez ©todos los derechos reservados
+          </div>
+        </div>
+      </footer>
+    </div>
