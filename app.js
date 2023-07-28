0057df59b943fc85e806923f19320e6192cd56ce
nestor12m <nestor20martinez@gmail.com>
2023-07-27 22:11:51 -0500

    0.0.1.5

---
+++ app.js
@@ -0,0 +1,8 @@
+const cambiarcolor = document.querySelector(".boton");
+const cambiar = document.querySelector(".titulo");
+
+cambiarcolor.addEventListener("click", function () {
+  cambiar.style.color = "red";
+});
+
+console.log("kmml");
